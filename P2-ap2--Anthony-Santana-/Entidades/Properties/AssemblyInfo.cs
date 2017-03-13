cbf62845cf4bb5afd017f58e75f763327ad5b3a4
Anthony  Santana <anthony26uu@gmail.com>
2017-03-13 00:23:29 -0400

    Entity y Proyects

--- P2-ap2--Anthony-Santana-/Entidades/Properties/AssemblyInfo.cs
+++ P2-ap2--Anthony-Santana-/Entidades/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BLL")]
+[assembly: AssemblyTitle("Entidades")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BLL")]
+[assembly: AssemblyProduct("Entidades")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("929c7092-38ad-4e21-95d4-b4affb418590")]
+[assembly: Guid("2928479e-4767-4bc3-95f3-81e6ad70dd63")]
 
 // Version information for an assembly consists of the following four values:
 //
