1c1a77e2d3b3056d293605ecfea3dc2b09bcfaeb
Anthony  Santana <anthony26uu@gmail.com>
2017-03-13 00:15:48 -0400

    Inicio

---
+++ P2-ap2--Anthony-Santana-/P2-ap2--Anthony-Santana-/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace P2_ap2__Anthony_Santana_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
